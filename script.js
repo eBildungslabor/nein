67a718c4dd30c3047d93c8bc62102132c23ba6cf
Nele Hirsch <nele@ebildungslabor.de>
2021-11-29 18:38:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-const nein = document.querySelector('.nein')
+const loveMe = document.querySelector('.loveMe')
 const times = document.querySelector('#times')
 
 let clickTime = 0
 let timesClicked = 0
 
-nein.addEventListener('click', (e) => {
+loveMe.addEventListener('click', (e) => {
     if(clickTime === 0) {
         clickTime = new Date().getTime()
     } else {
         if((new Date().getTime() - clickTime) < 800) {
-            createstopp(e)
+            createHeart(e)
             clickTime = 0
         } else {
             clickTime = new Date().getTime()
@@ -17,10 +17,10 @@ nein.addEventListener('click', (e) => {
     }
 })
 
-const createstopp = (e) => {
-    const stopp = document.createElement('i')
-    stopp.classList.add('fas')
-    stopp.classList.add('fa-stop-circle')
+const createHeart = (e) => {
+    const heart = document.createElement('i')
+    heart.classList.add('fas')
+    heart.classList.add('fa-heart')
 
     const x = e.clientX
     const y = e.clientY
@@ -34,9 +34,9 @@ const createstopp = (e) => {
     heart.style.top = `${yInside}px`
     heart.style.left = `${xInside}px`
 
-    nein.appendChild(stopp)
+    loveMe.appendChild(heart)
 
     times.innerHTML = ++timesClicked
 
-    setTimeout(() => stopp.remove(), 1000)
+    setTimeout(() => heart.remove(), 1000)
 }
